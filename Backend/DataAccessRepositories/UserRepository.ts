3f1a393e39c806ecfa4173c8b1d7d4999293b4cb
GermanBassanoCDS <gbassano@codigodelsur.com>
2024-07-24 14:05:47 -0300

    hashing of passwords implemented for future possible features

--- Backend/DataAccessRepositories/UserRepository.ts
+++ Backend/DataAccessRepositories/UserRepository.ts
@@ -46,11 +46,23 @@ class UserRepository implements IUserRepository {
     });
   }
 
+
+
   private mapToPrisma(user: Omit<User, 'id' | 'createdAt' | 'updatedAt' | 'contacts' | 'notes' | 'logins'>): Omit<PrismaUser, 'id' | 'createdAt' | 'updatedAt'> {
     return {
       name: user.getName() ?? '',
-      password: user.getPassword() ?? ''
+      password: this.simpleHashPass(user.getPassword()) ?? ''
     };
+  }
+
+  private simpleHashPass(str: string): string {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash |= 0;
+    }
+    return hash.toString(16);
   }
 
   async createUser(user: Omit<User, 'id' | 'createdAt' | 'updatedAt' | 'contacts' | 'notes' | 'logins'>): Promise<User> {
